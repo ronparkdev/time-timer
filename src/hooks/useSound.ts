0f1112258767cf75b50cc9175edba3ebaca49571
Ron Park <ronpark.dev@gmail.com>
2022-07-15 07:30:14 +0900

    Fix sound not working in old browser (safari iOS7)

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -6,6 +6,7 @@ const useSound = (soundOn: boolean) => {
   const bufferMapRef = useRef<{ [src: string]: AudioBuffer }>({})
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null)
   const [needUserInteraction, setNeedUserInteraction] = useState<boolean>(false)
+  const [failedSources, setFailedSources] = useState<string[]>([])
 
   useEffect(() => {
     if (audioContext?.state === 'suspended') {
@@ -36,25 +37,29 @@ const useSound = (soundOn: boolean) => {
 
       const buffer = await fetch(src)
         .then((res) => res.arrayBuffer())
-        .then((buffer) => audioContext.decodeAudioData(buffer))
-      bufferMapRef.current[src] = buffer
+        .then((buffer) => new Promise<AudioBuffer>((resolve) => audioContext.decodeAudioData(buffer, resolve)))
 
-      return buffer
+      return (bufferMapRef.current[src] = buffer)
     },
     [bufferMapRef],
   )
 
   const play = useCallback(
     async (src: string) => {
-      if (audioContext) {
-        const source = audioContext.createBufferSource()
-        const buffer = await fetchBuffer(src, audioContext)
-        source.buffer = buffer
-        source.connect(audioContext.destination)
-        source.start()
+      if (audioContext && !failedSources.includes(src)) {
+        try {
+          const source = audioContext.createBufferSource()
+          const buffer = await fetchBuffer(src, audioContext)
+          source.buffer = buffer
+          source.connect(audioContext.destination)
+          source.start()
+        } catch (e) {
+          setFailedSources((prevSources) => [...prevSources, src])
+          window.document.body.innerText = (e as unknown as any).stack
+        }
       }
     },
-    [audioContext, fetchBuffer],
+    [failedSources, audioContext, fetchBuffer],
   )
 
   return { play, handleAfterUserInteraction: needUserInteraction ? handleAfterUserInteraction : null }
